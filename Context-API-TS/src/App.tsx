e85bf385480e58d98acf7fb3a6296368a5c061bd
Het <hra.opash@gmail.com>
2026-01-22 12:28:37 +0530

    app

--- Context-API-TS/src/App.tsx
+++ Context-API-TS/src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import Header from './components/Header'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm";
+import Home from "./components/Home";
+import Header from "./components/Header";
 
-const App:React.FC = () => {
+const App: React.FC = () => {
   return (
-    <>
-      <Header/>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100 flex flex-col">
+        <Header />
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<LoginForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
